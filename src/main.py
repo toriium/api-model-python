997a8faa3e9f9cf93a038b1180045220c4183296
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-10-08 15:08:13 -0300

    feat: FastAPIEnv

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-import os
-import sys
-
-from src.infrastructure.db_orm.init_db import init_database
-from src.fastapi_app import get_fastapi_app
 import uvicorn
 
-
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.settings import FastAPIEnv
+from src.infrastructure.db_orm.init_db import init_database
+from src.fastapi_app import get_fastapi_app
 from src.infrastructure.db_orm.populate_db import populate_db
 
 
@@ -21,11 +17,11 @@ if __name__ == '__main__':
     app = get_fastapi_app()
     uvicorn.run(
         app=app,
-        host="0.0.0.0",
-        port=8080,
+        host=FastAPIEnv.APP_HOST,
+        port=FastAPIEnv.APP_PORT,
         debug=False,
         log_level='info',
         access_log=True,
-        workers=1,
+        workers=FastAPIEnv.APP_WORKERS,
         timeout_keep_alive=100
     )
--- src/settings.py
+++ src/settings.py
@@ -1,17 +1,15 @@
 import os
 
 from dotenv import load_dotenv
-from pydantic import BaseModel
 
 load_dotenv('../env.env')
 
-#
-# class DatabaseEnv:
-#     DB_HOST: str = os.getenv('DB_HOST')
-#     DB_USER: str = os.getenv('DB_USER')
-#     DB_PORT: str = os.getenv('DB_PORT')
-#     DB_NAME: str = os.getenv('DB_NAME')
-#     DB_PASSWORD: str = os.getenv('DB_PASSWORD')
+
+class FastAPIEnv:
+    APP_HOST: str = os.getenv('APP_HOST')
+    APP_PORT: int = int(os.getenv('APP_PORT'))
+    APP_WORKERS: int = int(os.getenv('APP_WORKERS'))
+
 
 class DatabaseEnv:
     DB_HOST: str = 'localhost'
