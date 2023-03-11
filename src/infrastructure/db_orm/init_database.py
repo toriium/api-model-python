93538a736ba493fac45658fbb8a9ad0b6506b4da
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2023-03-11 19:53:46 -0300

    fix: jaeger on container

--- src/infrastructure/db_orm/init_database.py
+++ src/infrastructure/db_orm/init_database.py
@@ -7,10 +7,16 @@ from src.settings import BASE_DIR
 
 
 def init_database():
-    time.sleep(10)
-    path = f"{BASE_DIR}/src/infrastructure/db_orm/alembic"
-    alembic_cfg = Config()
-    alembic_cfg.set_main_option("script_location",path)
-    alembic_cfg.set_main_option("sqlalchemy.url", get_writing_db_url())
+    for _ in range(5):
+        time.sleep(1)
+        try:
+            path = f"{BASE_DIR}/src/infrastructure/db_orm/alembic"
+            alembic_cfg = Config()
+            alembic_cfg.set_main_option("script_location",path)
+            alembic_cfg.set_main_option("sqlalchemy.url", get_writing_db_url())
+            command.upgrade(alembic_cfg, "head")
+            break
+        except:
+            ...
 
-    command.upgrade(alembic_cfg, "head")
+
--- src/tracing.py
+++ src/tracing.py
@@ -12,19 +12,20 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from src.infrastructure.db_orm.connection import reading_engine
+from src.settings import JaegerEnv
 
 
 class OTLPProvider:
     def __init__(self):
         # set the service name to show in traces
-        resource = Resource(attributes={"service.name": "service_fastapi"})
+        resource = Resource(attributes={"service.name": JaegerEnv.JAEGER_SERVICE_NAME})
 
         # set the tracer provider
         self.__tracer_provider = TracerProvider(resource=resource)
         trace.set_tracer_provider(tracer_provider=self.__tracer_provider)
 
         # set the processor
-        otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+        otlp_exporter = OTLPSpanExporter(endpoint=f"http://{JaegerEnv.JAEGER_HOST}:{JaegerEnv.JAEGER_PORT}", insecure=True)
         span_processor = BatchSpanProcessor(otlp_exporter)
         self.__tracer_provider.add_span_processor(span_processor=span_processor)
 
